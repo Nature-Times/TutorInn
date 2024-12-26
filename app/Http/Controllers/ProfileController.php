420fb76c74c4e83470466003b17cab3b9edffb92
Nature-Times <122514986+Nature-Times@users.noreply.github.com>
2024-12-27 03:16:48 +0700

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -30,11 +30,20 @@ class ProfileController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255|unique:users,email,',
             'age' => 'required|integer',
-            'phone' => 'required|max:255'
+            'phone' => 'required|max:255',
+            'profilePicture' => 'image|mimes:jpeg,png,jpg,gif|max:2048'
         ]);
 
         $user = Auth::user()->load('phones');
-        $user->update($request->only('name', 'email', 'age', 'phone'));
+        if ($request->hasFile('profilePicture')) {
+            // Store the file in the 'public/profile_pictures' directory
+            $filePath = $request->file('profilePicture')->store('profilePicture', 'public');
+
+            // Save the file path in the database
+            $user->profilePicture = $filePath;
+            $user->save();
+        }
+        $user->update($request->only('name', 'email', 'age', 'phone', 'profilePicture')); // Add other fields as needed
 
         return redirect()->route('update.update')->with('success', 'Profile updated successfully.');
     }
